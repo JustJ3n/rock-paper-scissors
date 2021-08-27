d366593712b27e5d2de676b2e1707adfe2ce034e
JustJ3n <jlee172754@gmail.com>
2021-08-26 21:29:05 -0400

    Add comment notes to each section about the purpose and use, made additional syntax correction

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
+//initialize the user and computer score to 0 
 let userScore=0; 
 let computerScore = 0; 
+
+//cache the DOM to have easier access by storing html elements into a variable name.
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".scoreboard");
-const result_p = document.querySelector("result > p");
+const result_p = document.querySelector(".result > p");
 const rock_div= document.getElementById("r");
 const paper_div= document.getElementById("p");
 const scissors_div= document.getElementById("s");
 
+//This function is to randomize the array of three choices: rock, paper, scissors. 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+// This function is to convert the choices letter rps to readable words - rock, paper, scissors.
 function convertToWord(letter){
     if(letter === "r") return "Rock";
     if(letter === "p") return "Paper";
     return "Scissors";
 }
 
+// user choice input
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
@@ -36,7 +42,7 @@ function lose(userChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
     const smallUserWord = "user".fontsize(3).sub();
     const smallCompWord = "comp".fontsize(3).sub();
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lost..💩`; 
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lost...💩`; 
 }
 
 function draw(userChoice, computerChoice) {
@@ -47,6 +53,7 @@ function draw(userChoice, computerChoice) {
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. It's a draw!`; 
 }
 
+// This function is to set game choice logic and list out the possibilities of choices and game result. 
 function game (userChoice) {
 const computerChoice = getComputerChoice();
 switch (userChoice + computerChoice) {
@@ -68,6 +75,7 @@ switch (userChoice + computerChoice) {
     }
 }
 
+// This function is to add click interactivity to the rock, paper, scissors buttons.
 function main() {
     rock_div.addEventListener('click', function(){
         game("r");
